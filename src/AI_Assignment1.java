5fbb01aa25d1375a61380b7bdb2579b79516111f
troublebob <robert.foran@ucdconnect.ie>
2014-10-30 14:59:33 +0000

    part 1

--- src/AI_Assignment1.java
+++ src/AI_Assignment1.java
@@ -2,7 +2,9 @@
 public class AI_Assignment1 {
 
 	public static void main(String[] args) {
+		Node n = new Node(3, 3, 7, 3, 3);
 		
+		System.out.println(n.toString(3));
 
 	}
 
--- src/Node.java
+++ src/Node.java
@@ -1,19 +1,29 @@
 import java.util.ArrayList;
-
+import java.util.Random;
 
 public class Node {
 
 	int estimated_score;
 	ArrayList<Node> ordered_children;
-	
+
 	public Node(int b, int d, int v, int i, int s) {
-		if (d != 0) {
+		ordered_children = new ArrayList<Node>(); 
+		int randomBranch = getRandomNumber(0,b);
+		if (d == 0){
+			this.estimated_score = v;
+		}else if (d != 0) {
+			this.estimated_score= v + getRandomNumber(i*-1,i);
 			for (int x = 0; x < b; x++) {
-				ordered_children.add(new Node(b, d - 1, v, i, s));
+				if (x == randomBranch){
+					ordered_children.add(new Node(b, d - 1, v * -1, i, s));
+				}else{
+					int newS = getRandomNumber(0, s);
+					ordered_children.add(new Node(b, d - 1, (v * -1) + newS , i, s));
+				}
 			}
 		}
 	}
-
+	
 	public int getEstimated_score() {
 		return estimated_score;
 	}
@@ -29,18 +39,27 @@ public class Node {
 	public void setOrdered_children(ArrayList<Node> ordered_children) {
 		this.ordered_children = ordered_children;
 	}
-	
-	public String toString(){
+
+	public int getRandomNumber(int l, int h){
+		Random rn = new Random();
+		return rn.nextInt(h-l) - 1;
+	}
+
+
+	public String toString(int d){
 		StringBuffer st = new StringBuffer();
-		st.append(estimated_score);
-		st.append('\t');
-		
-		for (int i = 0; i < ordered_children.size(); i++) {
-			st.append('-');
-			st.append(ordered_children.get(i).toString());
+		for(int i = d; i > 0; i--){
+			st.append('\t');
 		}
-	
+		st.append('*');
+		st.append(estimated_score);
+		st.append('\n');
+		for(int i = 0; i <ordered_children.size(); i++){
+			st.append(ordered_children.get(i).toString(d-1));
+		}
+
+
 		return st.toString();
-		
+
 	}
 }
