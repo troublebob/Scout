d8d8ad1692e3450cb825607e1fc7a4592b3f8933
troublebob <robert.foran@ucdconnect.ie>
2014-11-07 14:47:31 +0000

    4 in progress

--- src/IterativeDeepeningPVR.java
+++ src/IterativeDeepeningPVR.java
@@ -1,4 +1,15 @@
 
-public class IterativeDeepeningPVR {
+public class IterativeDeepeningPVR extends NegamaxNode {
+
+	public IterativeDeepeningPVR(int b, int d, int v, int i, int s, boolean isRoot) {
+		super(b, d, v, i, s, isRoot);
+	}
+	
+	public int searchAlgorithm(Node n,int depth){
+		int alpha =-2147483648;
+		int beta = 2147483647;
+		int score = computeNegamax(, score, score, score, false);
+		
+	}
 
 }
